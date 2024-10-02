2a133728e1a7f2f95d244390cc2c2362daebe8a2
PrasannaKumar-99 <prasannakumar8685@gmail.com>
2024-10-02 09:17:22 +0530

    Curd_project

---
+++ curd_project/src/main/java/com/example/Curd/CurdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdProjectApplication.class, args);
+	}
+
+}
---
+++ curd_project/src/test/java/com/example/Curd/CurdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Curd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
